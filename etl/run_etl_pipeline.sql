a7a854bd712db1bc26ecd0d39326d9e7e2963ce9
amit singh <amitms2005@gmail.com>
2026-04-01 16:47:30 -0500

    oracle tables modified and etl corrected

--- etl/run_etl_pipeline.sql
+++ etl/run_etl_pipeline.sql
@@ -1,26 +1,28 @@
 BEGIN
      -- Load Dim_Patient
     INSERT INTO Dim_Patient(patient_id,age,gender)
-    SELECT src_patient_id, FLOOR(MONTHS_BETWEEN(SYSDATE,dob)/12), gender
-    FROM STG_Patients;
+    SELECT patient_id, age, gender
+    FROM Patients;
 
     -- Load Dim_Doctor
     INSERT INTO Dim_Doctor(doctor_id,specialization)
-    SELECT src_doctor_id,specialization FROM STG_Doctors;
+    SELECT doctor_id,specialization FROM Doctors;
 
     -- Load Dim_Date
     INSERT INTO Dim_Date(date_key,the_date,month,year)
     SELECT TO_NUMBER(TO_CHAR(visit_date,'YYYYMMDD')), visit_date, TO_NUMBER(TO_CHAR(visit_date,'MM')), TO_NUMBER(TO_CHAR(visit_date,'YYYY'))
-    FROM STG_Visits;
+    FROM Visits;
 
     -- Load Fact_Visits
     INSERT INTO Fact_Visits(patient_key,doctor_key,date_key,total_cost)
     SELECT dp.patient_key, dd.doctor_key, dt.date_key, SUM(st.cost)
-    FROM STG_Visits sv
-    JOIN STG_Treatments st ON sv.src_visit_id = st.src_visit_id
-    JOIN Dim_Patient dp ON dp.patient_id = sv.src_patient_id
-    JOIN Dim_Doctor dd ON dd.doctor_id = sv.src_doctor_id
+    FROM VISITS sv
+    JOIN TREATMENTS st ON sv.visit_id = st.visit_id
+    JOIN Dim_Patient dp ON dp.patient_id = sv.patient_id
+    JOIN Dim_Doctor dd ON dd.doctor_id = sv.doctor_id
     JOIN Dim_Date dt ON dt.the_date = sv.visit_date
     GROUP BY dp.patient_key, dd.doctor_key, dt.date_key;
 END;
+commit;
+EXCEPTION WHEN OTHERS THEN ROLLBACK;
 /
---
+++ sql/03_transfrom_tables.sql
@@ -0,0 +1,35 @@
+BEGIN
+     -- transform Patient
+    INSERT INTO PATIENTs(patient_id,name,dob,age,gender)
+    SELECT
+        SUBSTR(patient_id, 1, 10)                            AS patient_id,
+        SUBSTR(NAME, 1, 100)                                 AS NAME,
+        TO_DATE(DOB, 'YYYY-MM-DD')                           AS DOB,
+        TRUNC(MONTHS_BETWEEN(SYSDATE, TO_DATE(DOB, 'YYYY-MM-DD')) / 12) AS AGE,
+        SUBSTR(GENDER, 1, 10)                                AS GENDER
+    FROM STG_PATIENTs
+    WHERE PATIENT_ID IS NOT NULL AND DOB IS NOT NULL
+    AND REGEXP_LIKE(DOB, '^\d{4}-\d{2}-\d{2}$');
+
+    -- transform Doctor
+    INSERT INTO DOCTORS(doctor_id,name, specialization)
+    SELECT doctor_id,name,specialization FROM STG_Doctors;
+
+    -- transform visits
+    INSERT INTO VISITS(VISIT_ID,PATIENT_ID,DOCTOR_ID,VISIT_DATE, DIAGNOSIS)
+    SELECT visit_id, patient_id, doctor_id,to_date(visit_date,'YYYY-MM-DD'), diagnosis 
+    FROM STG_Visits;
+
+   -- transform treatments
+    INSERT INTO TREATMENTS(TREATMENT_ID,VISIT_ID,TREATMENT_TYPE, COST)
+    SELECT TREATMENT_ID,VISIT_ID, TREATMENT_TYPE, TO_NUMBER(COST)
+    FROM STG_TREATMENTS;
+    
+    -- transform billing
+    INSERT INTO BILLING (bill_id,visit_id, total_amount, insurance_provider)
+    SELECT bill_id, visit_id, to_number(total_amount), insurance_provider 
+    FROM STG_Billing;
+END;
+commit;
+EXCEPTION WHEN OTHERS THEN ROLLBACK;
+/   
